e1bb2b824cbd89035ff69dce0c2c9433949a53bd
Weiyi Zhang <weiyi.zhang@geneegroup.com>
2014-07-23 17:05:09 +0800

    Replace npm with cnpm.

--- template/bin/init-glogon-server.sh
+++ template/bin/init-glogon-server.sh
@@ -3,6 +3,6 @@
 d=$DOCKER_GLOGON_SERVER_DIR
 if [ -e "$d" ]; then
 	chown -R genee:admin "$d"
-	su -l -s /bin/sh -c "cd $d && npm install" genee
+	su -l -s /bin/sh -c "cd $d && cnpm install" genee
 	/bin/sh -c "cd $d && ./deploy.sh"
 fi
--- template/bin/init-node-lims2.sh
+++ template/bin/init-node-lims2.sh
@@ -4,7 +4,7 @@ d=$DOCKER_NODE_LIMS2_DIR
 
 if [ -e "$d" ]; then
 	chown -R genee:admin "$d"
-	su -l -s /bin/sh -c "cd $d && npm install" genee
+	su -l -s /bin/sh -c "cd $d && cnpm install" genee
 	/bin/sh -c "cd $d && ./deploy.sh"
 fi
 
