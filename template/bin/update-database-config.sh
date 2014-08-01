ab3ff7a02245aed32be689e6736c955a7a443dfe
Weiyi Zhang <weiyi.zhang@geneegroup.com>
2014-08-01 10:07:10 +0800

    Configure lims2 password.

--- template/bin/update-database-config.sh
+++ template/bin/update-database-config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -i "s|\(\$config\['root'\]=.*\)://\(genee.*@[^/]*\)/|\1://genee:${GENEE_PASSWORD}@mysql/|" ${DOCKER_LIMS2_DIR}/application/config/database.php
+sed -i "s|\(\$config\['root'\]=.*\)://\(genee.*@[^/]*\)/|\1://genee:${GENEE_PASSWORD:-83719730}@mysql/|" ${DOCKER_LIMS2_DIR}/application/config/database.php
