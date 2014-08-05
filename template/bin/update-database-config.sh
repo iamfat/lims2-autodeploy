b836da0e282ef9b9783542ab547a59898816962d
Weiyi Zhang <weiyi.zhang@geneegroup.com>
2014-08-05 11:25:46 +0800

    Also modify lab specific database configuration.

--- template/bin/update-database-config.sh
+++ template/bin/update-database-config.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-sed -i "s|\(\$config\['root'\]=.*\)://\(genee.*@[^/]*\)/|\1://genee:${GENEE_PASSWORD:-83719730}@mysql/|" ${DOCKER_LIMS2_DIR}/application/config/database.php
+REGEXP="s|\(\$config\['root'\]=.*\)://\(genee.*@[^/]*\)/|\1://genee:${GENEE_PASSWORD:-83719730}@mysql/|"
+
+CONFIG_FILE_FMT="${DOCKER_LIMS2_DIR}/%s/config/database.php"
+
+sed -i $REGEXP "$(printf ${CONFIG_FILE_FMT} application)"
+
+LAB_CONFIG="$(printf ${CONFIG_FILE_FMT} sites/${SITE_ID}/labs/${LAB_ID})"
+[ ! -w $LAB_CONFIG ] || sed -i $REGEXP $LAB_CONFIG
