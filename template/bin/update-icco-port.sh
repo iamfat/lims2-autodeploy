c847cec7d66fe1b716edb0cfda5718eaa7845165
Weiyi Zhang <weiyi.zhang@geneegroup.com>
2014-08-08 15:35:05 +0800

    Publish icco ports.

---
+++ template/bin/update-icco-port.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+ICCO_CONFIG_FILE=${DOCKER_ICCO_SERVER_DIR}/config/default.js
+[ ! -r $ICCO_CONFIG_FILE ] || {
+	sed -i "
+	    s/\(\"port\"[^0-9]*\)[0-9]*/\1${DOCKER_ICCO_SERVER_PORT}/
+	    s/\(\"agent_port\"[^0-9]*\)[0-9]*/\1${DOCKER_ICCO_AGENT_PORT}/
+	" $ICCO_CONFIG_FILE
+}
+
+supervisorctl restart icco-server icco-agent
