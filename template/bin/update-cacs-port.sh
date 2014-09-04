db957810db21f34798945762bedfcde99835e613
Weiyi Zhang <weiyi.zhang@geneegroup.com>
2014-09-04 22:37:23 +0800

    Update port in docker image.

--- template/bin/update-cacs-port.sh
+++ template/bin/update-cacs-port.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-CACS_CONFIG_FILE=${DOCKER_CACS_SERVER_DIR}/config/default.js
-[ ! -r $CACS_CONFIG_FILE ] || sed -i "s/\(port:[^0-9]*\)[0-9]*\"/\1${DOCKER_CACS_PORT}\"/" $CACS_CONFIG_FILE
-
 REGEXP="s/\(\['cacs'\]\['port'\] *= *\)[0-9]*/\1${CACS_PUB_PORT}/"
 GLOBAL_CONFIG=$DOCKER_LIMS2_DIR/modules/equipments/config/device.php
 [ ! -r "$GLOBAL_CONFIG" ] || sed -i "$REGEXP" $GLOBAL_CONFIG
@@ -12,5 +9,3 @@ LAB_CONFIG=$DOCKER_LIMS2_DIR/sites/$SITE_ID/labs/$LAB_ID/config/device.php
 [ ! -r "$LAB_CONFIG" ] || sed -i "$REGEXP" $LAB_CONFIG
 
 [ ! -r /etc/xinetd.d/lims_device ] || sed -i '/service device_cacs/,/\}/d' /etc/xinetd.d/lims_device
-
-supervisorctl restart cacs-server
