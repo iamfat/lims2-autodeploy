862b0c82c0d05ff9e16b2303da50631419ae6d80
Weiyi Zhang <weiyi.zhang@geneegroup.com>
2014-10-21 13:46:05 +0800

    Run `create-orm-tables` even if backup file is present.

--- template/bin/init-db.sh
+++ template/bin/init-db.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 php ${DOCKER_LIMS2_DIR}/cli/create_orm_tables.php
-LIMS2_GENEE_EMAIL=$(date '+%s@geneegroup.com')
-php ${DOCKER_LIMS2_DIR}/cli/add_user.php 'genee|database' <<EOF
+
+if [ x"$1" = x--add-user ]; then
+	LIMS2_GENEE_EMAIL=$(date '+%s@geneegroup.com')
+	php ${DOCKER_LIMS2_DIR}/cli/add_user.php 'genee|database' <<EOF
 $LIMS2_GENEE_EMAIL
 
 ${LIMS2_GENEE_PASSWORD:-123456}
 EOF
+fi
